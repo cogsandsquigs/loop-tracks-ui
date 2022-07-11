5fbbb7eb92c09b2603ecfaa6261fe082a6101b1c
Ian Pratt <ianjdpratt@gmail.com>
2022-07-11 11:31:23 -0500

    changes

--- loop-tracks-ui/ContentView.swift
+++ loop-tracks-ui/ContentView.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorSelection = "pink";
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .padding()
+            Text("Select a train line color corresponding to the flashing Argon:")
+                .bold()
+            Picker("Select a train line color", selection: $colorSelection) {
+                Text("pink").tag("pink")
+                Text("red").tag("red")
+                Text("orange").tag("orange")
+                Text("green").tag("green")
+                Text("blue").tag("blue")
+                Text("purple").tag("purple")
+                Text("brown").tag("brown")
+            }
+                .pickerStyle(MenuPickerStyle())
+            Text(colorSelection)
         }
     }
 }
