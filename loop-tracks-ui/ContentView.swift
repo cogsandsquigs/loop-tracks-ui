771c4a3ba2df8f03e198fbecc2ae31878bc9eb28
Ian Pratt <ianjdpratt@gmail.com>
2022-07-11 11:00:33 -0500

    Initial Commit

---
+++ loop-tracks-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loop-tracks-ui
+//
+//  Created by admin on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loop-tracks-ui/loop_tracks_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  loop_tracks_uiApp.swift
+//  loop-tracks-ui
+//
+//  Created by admin on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct loop_tracks_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
